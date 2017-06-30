2e56f8bf7ce2605b0555a8d569f90611c34be526
mignonstyle <mignonxstyle@gmail.com>
2017-06-30 18:14:44 +0900

    first commit

---
+++ wp-simple-plugin.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Plugin Name: WordPress Simple Plugin
+ * Plugin URI:
+ * Description: Plugin to explain WordPress Plugin Basics
+ * Version: 1.0.0
+ * Author: Mignon Style
+ * Author URI:
+ * License: GPLv2
+ */
+
+defined( 'ABSPATH' ) or die( "Access denied !" );
